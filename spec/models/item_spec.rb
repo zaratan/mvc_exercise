8768847cc2a5a48e626c955e59edc6eb8440b6e2
stephanesdr <stadlerstephane@gmail.com>
2019-04-08 20:20:53 +0200

    rectify syntax update to updated

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe Item, type: :model do
       it { is_expected.to have_db_column(:id).of_type(:integer) }
       it { is_expected.to have_db_column(:original_price).of_type(:float).with_options(null: false) }
       it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
       it { is_expected.to have_db_column(:discount_percentage).of_type(:integer).with_options(default: 0) }
       it { is_expected.to have_db_column(:has_discount).of_type(:boolean).with_options(default: false) }
     end
