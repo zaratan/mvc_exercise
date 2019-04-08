793ff6b093f2ea8a9e35a6e9c9e3504def4f3273
stephanesdr <stadlerstephane@gmail.com>
2019-04-08 19:44:58 +0200

    add to items factory item_without_discount

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -24,6 +24,6 @@ FactoryBot.define do
       has_discount { false }
     end
     factory :item_with_discount, traits: %i[with_discount]
-    factory :item_whithout_discount, traits: %i[without_discount]
+    factory :item_without_discount, traits: %i[without_discount]
   end
 end
