d5ce1b2ef0d7f7073aeb256db6498db9e6d5de52
Denis Zaratan Pasin <denis.pasin@autodesk.com>
2019-04-12 13:23:51 +0900

    Devise

---
+++ spec/support/devise_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module DeviseHelper
+  def test_user
+    @test_user ||= build(:user)
+  end
+
+  def test_admin
+    @test_admin ||= build(:admin)
+  end
+
+  def sign_in_fake(user = test_user)
+    if user.nil?
+      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, scope: :user)
+      allow(controller).to receive(:current_user).and_return(nil)
+    else
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(user.is_a?(Admin) ? :current_admin : :current_user).and_return(user)
+    end
+  end
+end
